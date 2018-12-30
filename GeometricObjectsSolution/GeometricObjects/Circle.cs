02eee4de589aaedf542641edf1ecbb035ea9a262
Jaki13 <zimmer.jakob@t-online.de>
2018-12-30 11:25:21 +0100

    bis 4.

---
+++ GeometricObjectsSolution/GeometricObjects/Circle.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjects
+{
+    public class Circle
+    {
+        private static int _CountCircles;
+        private int _XCoordinate { get; set; }
+        private int _YCoordinate { get; set; }
+        private double _Radius;
+
+        public Circle()
+        {
+            _CountCircles++;
+        }
+
+        public Circle(double radius) : this()
+        {
+            Radius = radius;
+        }
+
+        public Circle(double radius, int xPos, int yPos) : this(radius)
+        {
+            XCoordinate = xPos;
+            YCoordinate = yPos;
+        }
+
+        public static int CountCircles { get => _CountCircles; }
+        public double Radius
+        {
+            get => _Radius;
+            set
+            {
+                if (value >= 0)
+                    _Radius = value;
+                else
+                    Console.WriteLine("Radius kan nicht negativ sein");
+            }
+        }
+
+        //Methoden
+        public double GetArea()
+        {
+            double area = 3.14 * Math.Pow(Radius, 2);
+            return area;
+        }
+
+        public static double GetArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public double GetCircumference()
+        {
+            double circumference = 2 * Radius * 3.14;
+            return circumference;
+        }
+
+        public static double GetCircumference(double radius)
+        {
+            return 2 * radius * Math.PI;
+        }
+
+        public void MoveXY(int dx, int dy)
+        {
+            XCoordinate += dx;
+            YCoordinate += dy;
+        }
+
+        public void MoveXY(int dx, int dy, int r)
+        {
+            XCoordinate += dx;
+            YCoordinate += dy;
+            Radius += r;
+        }
+
+
+        public int Bigger(Circle kreis)
+        {
+            if (Radius > kreis.Radius)
+                return 1;
+            else if (Radius == kreis.Radius)
+                return 0;
+            else
+                return -1;
+        }
+
+        public static int Bigger(Circle kreis1, Circle kreis2)
+        {
+            if (kreis1.Radius > kreis2.Radius)
+                return 1;
+            else if (kreis1.Radius == kreis2.Radius)
+                return 0;
+            else
+                return -1;
+        }
+    }
+}
---
+++ GeometricObjectsSolution/GeometricObjects/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle kreis = new Circle { Radius = 10, XCoordinate = -23, YCoordinate = 14};
+            kreis.Radius = 11;
+            Console.WriteLine("Der Keisradius beträgt {0} Einheiten.", kreis.Radius);
+
+            double area = kreis.GetArea();
+            Console.WriteLine("Der Flächeninhalt beträgt {0} Einheiten^2", area);
+
+            Console.WriteLine("Der Umfang beträgt {0} Einheiten", kreis.GetCircumference());
+
+            kreis.MoveXY(51, -15);
+
+            Console.WriteLine("X = {0}\nY = {1}", kreis.XCoordinate, kreis.YCoordinate);
+
+            Circle kreis2 = new Circle { Radius = 7, XCoordinate = 1, YCoordinate = 0 };
+
+            Console.WriteLine("Kreis größer als Kreis2? : " + kreis.Bigger(kreis2));
+
+            Console.Read();
+
+           
+        }
+    }
+}
